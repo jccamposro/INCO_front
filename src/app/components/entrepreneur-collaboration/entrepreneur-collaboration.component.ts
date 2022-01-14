d68a337d30bfe06e63bb1ea9a00cb4ab455cfb3e
Nicolas Molano <nmolanob@unal.edu.co>
2022-01-14 12:30:31 -0500

    js entrepreneur-collaboration

--- src/app/components/entrepreneur-collaboration/entrepreneur-collaboration.component.ts
+++ src/app/components/entrepreneur-collaboration/entrepreneur-collaboration.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ScriptsLoadService } from 'src/app/scripts-load.service';
 
 @Component({
     selector: 'app-entrepreneur-collaboration',
     templateUrl: './entrepreneur-collaboration.component.html',
-    styleUrls: [ './entrepreneur-collaboration.component.css' ]
+    styleUrls: ['./entrepreneur-collaboration.component.css']
 })
 export class EntrepreneurCollaborationComponent implements OnInit {
 
-    constructor() {
+    constructor(private _ScriptLoad: ScriptsLoadService) {
+        _ScriptLoad.load(["userView"])
     }
 
     ngOnInit(): void {
-    }
+}
 
 }
